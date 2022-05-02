0b0041e18705b0d5a8e6b38fdbc1a9a297f51c00
Songsong <614750803@qq.com>
2022-05-02 23:12:27 +0800

    deploy

---
+++ precache-manifest.09b98f4ed51450b78df4b0eac4048aa9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85f6c1f74040373ed89304629958876",
+    "url": "/bs-website/index.html"
+  },
+  {
+    "revision": "a02ce97d3327bd928afb",
+    "url": "/bs-website/static/css/2.20d67f8f.chunk.css"
+  },
+  {
+    "revision": "0c46be6b6a618defd366",
+    "url": "/bs-website/static/css/main.c7ef892c.chunk.css"
+  },
+  {
+    "revision": "a02ce97d3327bd928afb",
+    "url": "/bs-website/static/js/2.42174909.chunk.js"
+  },
+  {
+    "revision": "caaaabb4ea68597c9425b89671ddac02",
+    "url": "/bs-website/static/js/2.42174909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c46be6b6a618defd366",
+    "url": "/bs-website/static/js/main.fd7b7a28.chunk.js"
+  },
+  {
+    "revision": "6bccf3d237dfc181400b",
+    "url": "/bs-website/static/js/runtime-main.a18153ad.js"
+  },
+  {
+    "revision": "af517725810d14c09f820a8a4ca22deb",
+    "url": "/bs-website/static/media/404.af517725.png"
+  }
+]);
